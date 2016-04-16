a43a54bc9722a9874be976012a5a2f499b886693
mehran63 <m.farshadmehr@sepanta.com>
2016-04-16 18:38:54 +1000

    Car List

---
+++ Web/ServiceContract/CarListItemDto.cs
@@ -0,0 +1,15 @@
+﻿using MicroCarSales.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroCarSales.Web.ServiceContract
+{
+    public class CarListItemDto
+    {
+        public Car Car { get; set; }
+
+        public Dealer Dealer { get; set; }
+    }
+}
